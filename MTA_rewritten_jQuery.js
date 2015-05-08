3ca4fc972163cfd9ca97494355c8076e9b9c7e12
Guillaume Perrin <gperrin01@gmail.com>
2015-05-08 14:06:19 +0100

    JS code cleaned dried

--- MTA_rewritten_jQuery.js
+++ MTA_rewritten_jQuery.js
@@ -1,5 +1,5 @@
-// function intersection  - was writting jScript indexOF on JQ ELEMENT!!!
-// When defining event listeners, will using $(.class) impact ALL items with that class, or only the one(s) impacted by the event
+// Q1 function intersection  - was writting jScript indexOF on JQ ELEMENT!!!
+// Q2  When defining event listeners, will using $(.class) impact ALL items with that class, or only the one(s) impacted by the event
 var Mta = {
   'line N': ['times square_N', '34th_N', '28th_N', '23rd_N', 'union square', '8th_N'],
   'line L': ['8th_L', '6th_L', 'union square', '3rd_L', '1st_L'],
@@ -17,14 +17,14 @@ $(document).ready(function() {
   createDropdowns();
   setUpEventListeners();
 })
+
 //EVENT LISTENERS
 var setUpEventListeners = function() {
-  $('#line_start').on('change', displayDestinationStart);
-  $('#line_end').on('change', displayDestinationEnd);
-  // Could this be grouped into one function?
+  $('.line').on('change', function(){
+    displayDestination($(this));
+  })
   $('#click_result').on('click', displayJourney)
 } // End event listenrers
-
 
 // functions for Dropdowns and Buttons
 function createDropdowns(){
@@ -32,28 +32,18 @@ function createDropdowns(){
     $('.line').append('<option>'+ Object.keys(Mta)[i]  + '</option>')
   }
 }
-function displayDestinationStart() {
-  //avoid adding up stops os multiple clcks
-  $('#station_start').children().remove();
-  $('#station_start').append('<option>Please select</option>');
-  // what line am I on? -> $('#station_start').val()
-  var hereLine = $('#line_start').val();
+function displayDestination($lineX) {
+  $lineX.next().empty();
+  $lineX.next().append('<option>Please select</option>');
+  var hereLine = $lineX.val();
   Mta[hereLine].forEach(function(MtaStop){
-    $('#station_start').append('<option>'+ MtaStop + '</option>');
-  })
-}
-function displayDestinationEnd() {
-  $('#station_end').empty();
-  $('#station_end').append('<option>Please select</option>');
-  var hereLine = $('#line_end').val();
-  Mta[hereLine].forEach(function(MtaStop){
-    $('#station_end').append('<option>'+ MtaStop + '</option>');
+    $lineX.next().append('<option>'+ MtaStop + '</option>');
   })
 }
 function displayJourney() {
   init_line = $('#line_start').val();
   init_stop = $('#station_start').val(); 
-  end_line = $('#station_end').val();
+  end_line = $('#line_end').val();
   end_stop = $('#station_end').val(); 
   $('#display_result').text(calcJourney(init_line, init_stop, end_line, end_stop));
 }
@@ -69,8 +59,7 @@ function calcJourney(init_line, init_stop, end_line, end_stop){
   }
   else {
     // the below only works because there is a single intersection between 2 lines
-    // var intersection =  findIntersection(Mta[init_line], Mta[end_line])[0]; 
-    var intersection = 'union square';
+    var intersection =  findIntersection(Mta[init_line], Mta[end_line])[0]; 
     var nstops_init_line = numberStops(init_line, init_stop, intersection);
     var nstops_end_line = numberStops(end_line, end_stop, intersection);
     nstops = nstops_init_line + nstops_end_line;
@@ -84,8 +73,7 @@ function findIntersection(line1, line2) {
   });
 };
 function numberStops(line, stop1, stop2){
-  // return Math.abs( Mta[line].indexOf(stop1) - Mta[line].indexOf(stop2) );
-  return Math.abs( $.inArray(stop1, Mta[line]) - $.inArray(stop2, Mta[line]) );
+  return Math.abs( Mta[line].indexOf(stop1) - Mta[line].indexOf(stop2) );
 }
 
 
@@ -102,7 +90,24 @@ function storeInLocalStorage(init_line, init_stop, end_line, end_stop) {
 }
 
 
-
+// function displayDestinationStart() {
+//   //avoid adding up stops os multiple clcks
+//   $('#station_start').empty();
+//   $('#station_start').append('<option>Please select</option>');
+//   // what line am I on? -> $('#station_start').val()
+//   var hereLine = $('#line_start').val();
+//   Mta[hereLine].forEach(function(MtaStop){
+//     $('#station_start').append('<option>'+ MtaStop + '</option>');
+//   })
+// }
+// function displayDestinationEnd() {
+//   $('#station_end').empty();
+//   $('#station_end').append('<option>Please select</option>');
+//   var hereLine = $('#line_end').val();
+//   Mta[hereLine].forEach(function(MtaStop){
+//     $('#station_end').append('<option>'+ MtaStop + '</option>');
+//   })
+// }
 
       // for (var k=0; k<Object.keys(Mta)[i].length; k++)
       // $('.station').append('<option>'+ Mta[Object.keys(Mta)[i]][k] + '</option>');
